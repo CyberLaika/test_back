4f87763e71b0bdba8ad4a7858009778dcae01624
Kirill <ston727577@gmail.com>
2023-10-14 14:06:04 +0300

    a

--- app.js
+++ app.js
@@ -2,12 +2,16 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+const http = require('http')
+const server = http.createServer(app)
+
 app.use(express.static('public'))
 
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/index.html')
 })
 
-app.listen(port, () => {
+server.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
+ 
