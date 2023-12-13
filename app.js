172ef92627b6220bdc5c7cfcae96d4725178944a
Kirill <ston727577@gmail.com>
2023-12-14 01:05:42 +0300

    Update app.js

--- app.js
+++ app.js
@@ -1,3 +1,6 @@
+import { createRequire } from "module";
+const require = createRequire(import.meta.url);
+
 const tf = require('@tensorflow/tfjs')
 require('@tensorflow/tfjs-node')
 const express = require('express')
